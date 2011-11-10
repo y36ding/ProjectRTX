faca5b0e22f4abd9b66286e288eb3731bcb812e1
Matt Ding <matt.ding@hotmail.com>
2011-11-09 18:30:43 -0600

    test again

---
+++ kbcrt.h
@@ -0,0 +1,26 @@
+// kb crt header file
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFERSIZE 100;
+
+// structures for shared memory
+#define MAXCHAR 80	
+
+typedef struct {
+	int ok_flag;
+	char indata[MAXCHAR];
+	int length;
+} inputbuf;
+
+typedef struct {
+	int ok_flag;
+	char outdata[MAXCHAR];
+	int length;
+} outputbuf;
---
+++ rtx.h
@@ -0,0 +1,13 @@
+// rtx.h
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "kbcrt.h"
+
+
